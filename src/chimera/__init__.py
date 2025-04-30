4de43b9602f408e5c97946576459a0c879217ff5
Amirofcodes <j.bouddehbine@it-students.fr>
2025-04-30 14:21:08 +0200

    Implement template refactor with component-based architecture (v0.2.0)

--- src/chimera/__init__.py
+++ src/chimera/__init__.py
@@ -2,6 +2,6 @@
 ChimeraStack CLI - A template-based development environment manager.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = "Amirofcodes"
 __email__ = "amirofcodes20@gmail.com"
