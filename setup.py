dbdb90b5c8ac5a7f7989729ad232523777f57e37
Amirofcodes <j.bouddehbine@it-students.fr>
2025-05-02 11:22:14 +0200

    fix: correct GitHub repository URL and bump version to 0.2.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="chimera-stack-cli",
-    version="0.2.2",
+    version="0.2.3",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True,
